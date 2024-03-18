3d5187a175249fe1ced8a1250250117f02097ff7
김소연 <gimsoyeon@gimsoyeon-ui-MacBookAir.local>
2024-03-19 00:04:28 +0900

    Initial commit

---
+++ src/main/java/org/example/Variables.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "Soyeon Kim";
+
+        System.out.println(name + "님, 배송이 시작됩니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+    }
+}
