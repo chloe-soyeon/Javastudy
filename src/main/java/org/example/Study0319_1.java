61c6396f971e24d86ad365cc5d90ef63075d2873
chloe-soyeon <soyeon96129@gmail.com>
2024-03-19 14:27:59 +0900

    0319_1

---
+++ src/main/java/org/example/Study0319_1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Study0319_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
