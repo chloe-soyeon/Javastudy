7fea19416f72ceb29711cb1a1398f9806f93be06
chloe-soyeon <soyeon96129@gmail.com>
2024-03-20 19:39:36 +0900

    While 반복문 사용

---
+++ src/main/java/org/example/Study0320.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Study0320 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while(i<5){
+            i = i + 1;
+            System.out.println(i);
+        }
+    }
+}
