a7edc0cf6c660081d791ca747e54c08c40f633a0
chloe-soyeon <soyeon96129@gmail.com>
2024-05-31 09:08:22 +0900

    Turtle_move

---
+++ src/main/java/org/example/Turtle_move.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Turtle_move {
+    public static void main(String[] args) {
+
+        Turtle turtle = new Turtle();
+
+            turtle.speed(600);
+
+            turtle.forward(100);
+
+            turtle.left(90);
+            turtle.forward(100);
+
+            turtle.left(90);
+            turtle.forward(100);
+
+            turtle.left(90);
+            turtle.forward(100);
+
+    }
+}
